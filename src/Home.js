22f9e695e63365c6fc7d06aae6bb97b8c050a8a6
Marta Kask <marta.m.kask@gmail.com>
2021-05-06 17:17:58 +0200

    minor fixes

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import BlogList from './BlogList';
 
 const Home = () => {
 
@@ -20,12 +21,7 @@ const Home = () => {
 
   return (
     <div className="home">
-    {blogs.map(blog => (
-      <div className="blog-preview" key={blog.id} >
-        <h2>{ blog.title }</h2>
-        <p>Written by { blog.author }</p>
-      </div>
-      ))}
+     <BlogList />
     </div>
   );
 }
