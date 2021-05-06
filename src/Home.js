1f351fbe27c157e0ccafe3c013c77c00d558ed82
Marta Kask <marta.m.kask@gmail.com>
2021-05-06 19:15:16 +0200

    created props

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,16 @@
-import { useState } from 'react'
-import BlogList from './BlogList';
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'React with Yoshi', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-  // let name = "mario"
-  // const [name, setName] = useState('mario');
-
- //  const handleClick = () => {
-  //  setName('luigi');
-  // }
-
-  const handleClickAgain = (name, e) => {
-    console.log('hello' + name, e.target);
-  }
+  ])
 
   return (
     <div className="home">
-     <BlogList />
+      <BlogList blogs={blogs} title="All Blogs" />
     </div>
   );
 }
